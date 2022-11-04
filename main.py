e0985c884b761a43652577df443dd100f3db9339
BmmNdr <abiemmibi@gmail.com>
2022-11-04 19:32:51 +0100

    Removed unused method and attributes

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import handTrackingModule
 
 
 def main():
+    # video capture
     cap = cv2.VideoCapture(0)
+    # hand tracking module
     tracker = handTrackingModule.handTracker()
 
     while True:
         success, image = cap.read()
         image = tracker.handsFinder(image)
-        lmList = tracker.positionFinder(image)
 
         cv2.imshow("Video", cv2.flip(image, 1))
         cv2.waitKey(1)
